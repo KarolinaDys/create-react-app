a06ba0fbd74147c7929f9c16615eb7fd6cde34ee
Karolina Dys <karolinadyss@gmail.com>
2020-10-23 12:33:25 +0200

    add full aplication todo

--- src/App.js
+++ src/App.js
@@ -1,50 +1,13 @@
 import React from "react";
+import './index.css';
+import TodoList from "./components/TodoList";
 
-function App(props) {
+function App() {
   return (
-    <div className="todoapp stack-large">
+    <div className="todo-app">
     
-      <h1>My Todo App</h1>
+      <TodoList/>
     
-      <form>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-          placeholder ="type a new todo"
-        />
-        </form>
-        <div className="button">
-      <button type="submit" className="btn btn__primary btn__lg">
-        Add Todo
-      </button>
-      </div>
-      <hr></hr>
-      <h2 id="list-heading">Todos</h2>
-      <ul
-        //role="list"
-        className="todo-list stack-large stack-exception"
-        aria-labelledby="list-heading"
-      >
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-0" type="checkbox" />
-            <label className="todo-label" htmlFor="todo-0">
-              Learn React
-            </label>
-          </div>
-        </li>
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-1" type="checkbox" />
-            <label className="todo-label" htmlFor="todo-1">
-              Be Awesome!
-            </label>
-          </div>
-        </li>
-      </ul>
     </div>
   );
 }
